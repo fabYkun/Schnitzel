f6d1a46a6e3a92128ad88e4536a60c9568504f42
fabY-kun <fabien.borel@epitech.eu>
2019-01-26 10:26:17 +0100

    dialog prefab and basic scriptable object

---
+++ Assets/Scripts/Gamemachine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class        Gamemachine : MonoBehaviour
+{
+
+    
+
+    enum Gamemachines
+    {
+        Titlescreen,
+        Game,
+        Endgame
+    };
+
+    enum Gamestate
+    {
+        Kitchen,
+        Dialogs
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SODescription.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Used for translation purposes, since its a ScriptableObject it can be serialized as JSON and referenced in any object
+/// </summary>
+[CreateAssetMenu(menuName = "Objects/SODescription")]
+public class                        SODescription : ScriptableObject
+{
+    /// <summary>Name/Title of the object/action/...thing</summary>
+    public string                   title;
+    /// <summary>Description of the object/action...thing</summary>
+    public string                   description;
+}
