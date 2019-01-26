f2830b0cade1d05e7a85d1162fcc4c7fc7167fbb
Quentin Harscoët <harscoet.quentin@gmail.com>
2019-01-26 19:11:48 +0100

    Reup placeholder assets

---
+++ Assets/FoodBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodBehaviour : MonoBehaviour
+{
+
+    public int test;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        newPos.z = 1.0f;
+        Debug.Log(newPos);
+        this.gameObject.transform.position = newPos;
+
+    }
+
+}
